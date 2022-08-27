f205f23b283dca6baac653cda614fd319d076e13
mishael dada <mishaeldada@gmail.com>
2022-08-27 15:17:54 +0100

    added icon

--- src/components/Navbar/Mobilenavbar.js
+++ src/components/Navbar/Mobilenavbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import {AiOutlineClose} from 'react-icons/ai'
+import { AiOutlineClose } from "react-icons/ai";
+import { FaInbox } from "react-icons/fa";
 
 function Modal({ handleClick }) {
   return (
@@ -8,8 +9,7 @@ function Modal({ handleClick }) {
       <div className="h-screen grid md:hidden fixed top-0 left-0 z-50 w-3/4 bg-white shadow-lg text-base border-2">
         <div className="flex items-center justify-start px-5 border-b-2 ">
           <span className="cursor-pointer" onClick={handleClick}>
-
-          <AiOutlineClose/>
+            <AiOutlineClose />
           </span>
           <Link to="/">
             <div className="ml-6 font-['Montserrat']">
@@ -20,32 +20,26 @@ function Modal({ handleClick }) {
         </div>
 
         <div className="p-5 border-b-2">
-          <div className="flex justify-between items-center">
-            <p className="uppercase font-bold text-[#051D4C]">my account</p>
-            <svg
-              className="w-2 h-4"
-              viewBox="0 0 9 15"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M0 13.5432V0.978508C0 0.108488 1.0519 -0.327205 1.66709 0.287981L7.94941 6.5703C8.33076 6.95165 8.33076 7.57001 7.94941 7.95136L1.66709 14.2337C1.0519 14.8489 0 14.4132 0 13.5432Z"
-                fill="#051D4C"
-              />
-            </svg>
-          </div>
+          <Link to="/login">
+            <div className="flex justify-between items-center">
+              <p className="uppercase font-bold text-[#051D4C]">my account</p>
+              <svg
+                className="w-2 h-4"
+                viewBox="0 0 9 15"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M0 13.5432V0.978508C0 0.108488 1.0519 -0.327205 1.66709 0.287981L7.94941 6.5703C8.33076 6.95165 8.33076 7.57001 7.94941 7.95136L1.66709 14.2337C1.0519 14.8489 0 14.4132 0 13.5432Z"
+                  fill="#051D4C"
+                />
+              </svg>
+            </div>
+          </Link>
           <div className="flex justify-start items-center pt-5 font-normal">
-            <svg
-              className="w-5 mr-4"
-              viewBox="0 0 25 25"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M24.8779 9.01367L22.4072 1.60156C22.0898 0.644531 21.1963 0 20.1855 0H13.2812V9.375H24.9365C24.917 9.25293 24.917 9.13086 24.8779 9.01367ZM11.7188 0H4.81445C3.80371 0 2.91016 0.644531 2.59277 1.60156L0.12207 9.01367C0.0830078 9.13086 0.0830078 9.25293 0.0634766 9.375H11.7188V0ZM0 10.9375V22.6562C0 23.9502 1.0498 25 2.34375 25H22.6562C23.9502 25 25 23.9502 25 22.6562V10.9375H0Z"
-                fill="black"
-              />
-            </svg>
+            <span className="w-4 mr-4">
+              <FaInbox />
+            </span>
             <p className="font-['Poppins']">Orders</p>
           </div>
         </div>
@@ -53,7 +47,9 @@ function Modal({ handleClick }) {
         <div className="p-5 border-b-2">
           <div className="flex justify-between items-center">
             <p className="uppercase font-bold text-[#051D4C]">Our Categories</p>
-            <p className="font-semibold">See all</p>
+            <Link to="/shop">
+              <p className="font-semibold">See all</p>
+            </Link>
           </div>
           <div className="pt-5 font-normal font-['Poppins']">
             <p>Male</p>
